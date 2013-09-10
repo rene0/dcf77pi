b1af9e481bb354a899f4a7fe31675595c24033be
René Ladan <rene@freebsd.org>
2013-09-10 13:07:55 +0200

    Revert "Explicitly flush the log file each new minute."
    This reverts commit 04c30d030781842a7a6fa3adbcbc2364dd4b890b.
    
    While the idea of an up-to-date log file is nice, over NFS this causes
    too much delay for bit 0 to be properly received.

--- input.c
+++ input.c
@@ -394,10 +394,8 @@ get_bit(void)
 report:
 		if (logfile) {
 			fprintf(logfile, "%c", outch);
-			if (state & GETBIT_EOM) {
+			if (state & GETBIT_EOM)
 				fprintf(logfile, "\n");
-				fflush(logfile);
-			}
 		}
 	} else {
 		while (!valid) {
