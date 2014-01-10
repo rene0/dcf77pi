b0e82b9cb94c0c89bd83ae59855a7ef50c4ec560
René Ladan <rene@freebsd.org>
2014-01-10 17:59:43 +0100

    Bump copyright year.

--- dcf77pi.c
+++ dcf77pi.c
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013 René Ladan. All rights reserved.
+Copyright (c) 2013-2014 René Ladan. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions
