ead4ceab34b99198ac834f1a18e5289596a574cc
René Ladan <r.c.ladan@gmail.com>
2014-06-23 13:12:19 +0200

    No need to check date/time further than hh:00 when processing a DST change, this is already done when setting the announcement flag.
    Drop 'tmp' variable.
    Simplify the guard for assigning newtime.tm_isdst (no need to check rval & DT_DSTERR when checking oldderr && ok)

--- decode_time.c
+++ decode_time.c
@@ -361,15 +361,11 @@ decode_time(int init, int minlen, uint8_t *buffer, struct tm *time,
 		 * initial state (otherwise DST would never be valid)
 		 * actually announced and time is Sunday, lastday, 01:00 UTC
 		 */
-		tmp = (announce & ANN_CHDST) &&
-		    (minlen == 59 || minlen == 60) && time->tm_min == 0 &&
-		    utchour == 1 && time->tm_wday == 7 &&
-		    time->tm_mday > lastday(*time) - 7 &&
-		    (time->tm_mon == summermonth || time->tm_mon == wintermonth);
-		if (tmp || (((rval & DT_DSTERR) == 0) &&
-		    ((olderr && ok) || (init & 1) == 1))) {
+		if (((announce & ANN_CHDST) && time->tm_min == 0) ||
+		    (olderr && ok) ||
+		    ((rval & DT_DSTERR) == 0 && (init & 1) == 1)) {
 			newtime.tm_isdst = buffer[17]; /* expected change */
-			if (tmp) {
+			if ((announce & ANN_CHDST) && time->tm_min == 0) {
 				announce &= ~ANN_CHDST;
 				rval |= DT_CHDST;
 			}
