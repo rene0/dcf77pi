1effca904f1cbd15ee71d8901e41390e80019cb9
René Ladan <r.c.ladan@gmail.com>
2014-12-21 16:08:49 +0100

    Fix commit 4b12212bcc2546edaf5d023033742fe1510fcd49

--- input.c
+++ input.c
@@ -593,7 +593,7 @@ get_bit_file(void)
 		TRYCHAR
 	}
 	if (!read_acc_minlen)
-		acc_minlen += 1000000 * bit.t / bit.realfreq * 1000;
+		acc_minlen += 1000000 * bit.t / (bit.realfreq / 1000);
 
 	return state;
 }
