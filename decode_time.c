21e93793ebf163272503fb0b5ecf5ecaa49f9060
Rene Ladan <r.c.ladan@gmail.com>
2017-11-26 18:50:40 +0100

    Fix a comment in handle_dst(), leftover from issue #24

--- decode_time.c
+++ decode_time.c
@@ -266,7 +266,7 @@ handle_dst(unsigned errflags, bool olderr, const int buffer[], struct tm time,
 	if (buffer[17] != time.tm_isdst || buffer[18] == time.tm_isdst) {
 		/*
 		 * Time offset change is OK if:
-		 * - announced and time is Sunday, lastday, 01:00 UTC
+		 * - announced and on the hour
 		 * - there was an error but not any more (needed if decoding at
 		 *   startup is problematic)
 		 * - initial state (otherwise DST would never be valid)
