275af5e48c92d5e59058b11fd4a9b90b4fbd1e6b
Rene Ladan <r.c.ladan@gmail.com>
2019-11-07 22:07:06 +0100

    dcf77pi-readpin: show bit value again in non-raw mode.

--- dcf77pi-readpin.c
+++ dcf77pi-readpin.c
@@ -112,8 +112,9 @@ main(int argc, char *argv[])
 		if (bit.marker == emark_toolong || bit.marker == emark_late) {
 			min++;
 		}
-		printf("%i %i %u %llu %llu %llu %i:%i\n", bi.tlow, bi.tlast0,
-		    bi.t, bi.bit0, bi.bit20, bi.realfreq, min, get_bitpos());
+		printf("%i %i %i %u %llu %llu %llu %i:%i\n", bit.bitval, bi.tlow,
+		    bi.tlast0, bi.t, bi.bit0, bi.bit20, bi.realfreq, min,
+		    get_bitpos());
 		if (bit.marker == emark_minute) {
 			min++;
 		}
